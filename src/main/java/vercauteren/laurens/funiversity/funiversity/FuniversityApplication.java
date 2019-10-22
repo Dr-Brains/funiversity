d53e412ebf54f0d077722514d7f22c72ebe24b7a
Laurens Vercauteren <laurens.vercauteren@gmail.com>
2019-10-22 09:10:23 +0200

    initial commit

---
+++ src/main/java/vercauteren/laurens/funiversity/funiversity/FuniversityApplication.java
@@ -0,0 +1,13 @@
+package vercauteren.laurens.funiversity.funiversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vercauteren/laurens/funiversity/funiversity/FuniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package vercauteren.laurens.funiversity.funiversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
